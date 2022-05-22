280c0f1b0e109c51afe32f0eca3e8739ca3d3e7e
egr125 <eoing125@gmail.com>
2022-05-22 16:18:50 +0100

    homr kill

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Live Demo with Eoin Graham </p>
             )}
             {items.map(item => (
                 <ItemDisplay
